a75ba164fa6d537044906954e8e7778590d4f960
ngangavic <ngangavictor10@gmail.com>
2020-03-09 11:58:57 +0300

    servlet form login

---
+++ Servelet/src/java/Login.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            response.setContentType("text/html");
+          
+            String name = request.getParameter("uname");//will return value  
+            out.println("Welcome " + name);
+
+            out.close();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "copyright 2020 .gitignore";
+    }
+
+}
