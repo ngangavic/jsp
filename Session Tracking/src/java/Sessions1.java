6399ecb655970f66e76b91af805f5f6ac1fe5deb
ngangavic <ngangavictor10@gmail.com>
2020-03-21 16:38:45 +0300

    Session tracking: Sessions

---
+++ Session Tracking/src/java/Sessions1.java
@@ -0,0 +1,47 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Sessions1 extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            response.setContentType("text/html");
+
+            String n = request.getParameter("uname");
+            out.print("Welcome " + n);
+
+            HttpSession session = request.getSession();
+            session.setAttribute("uname", n);
+
+            out.print("<a href='Sessions2'>visit</a>");
+
+            out.close();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
